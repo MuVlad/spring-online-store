4ea7c9733a6faa46dd2ae12c15bbb721591ec6d5
Vlad Muslimov <muslimov.vlad@mail.ru>
2024-03-02 18:17:22 +0500

    Initial commit

---
+++ src/main/java/com/muslimov/vlad/springonlinestore/SpringOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package com.muslimov.vlad.springonlinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOnlineStoreApplication.class, args);
+	}
+
+}
